9ad67ed284168db5af11d35f4ee6dc0b130ef449
Alan Somers <asomers@gmail.com>
2023-07-27 15:24:20 -0600

    Add degraded cases to the vdev_raid torture test.

--- bfffs-core/tests/torture/vdev_raid.rs
+++ bfffs-core/tests/torture/vdev_raid.rs
@@ -1,3 +1,6 @@
+//! Write and read data to a raid device using a random pattern, and verify
+//! integrity.
+
 use std::{
     fs,
     mem,
@@ -23,12 +26,13 @@ use bfffs_core::{
     BYTES_PER_LBA,
     LbaT,
     mirror::Mirror,
-    raid::{self, VdevRaidApi},
+    raid::{self, Manager, VdevRaidApi},
 };
 
 struct Harness {
     vdev: Arc<dyn VdevRaidApi>,
     _tempdir: TempDir,
+    paths: Vec<PathBuf>,
     k: i16,
     f: i16,
     chunksize: LbaT,
@@ -52,7 +56,7 @@ async fn harness(n: i16, k: i16, f: i16, chunksize: LbaT) -> Harness {
     ).collect::<Vec<_>>();
     let cs = NonZeroU64::new(chunksize);
     let vdev = raid::create(cs, k, f, mirrors);
-    Harness{vdev, _tempdir: tempdir, k, f, chunksize}
+    Harness{vdev, _tempdir: tempdir, paths, k, f, chunksize}
 }
 
 /// Create a buffer with deterministic contents corresponding to the given file
@@ -66,8 +70,156 @@ fn mkbuf(offs: LbaT, len: usize) -> Vec<u8> {
     }).collect::<Vec<_>>()
 }
 
-/// Write and read data to a raid device using a random pattern, and verify
-/// integrity.
+async fn do_test(
+    vdev: Arc<dyn VdevRaidApi>,
+    chunksize: LbaT,
+    k: i16,
+    f: i16,
+    seed: Option<[u8; 16]>)
+{
+    let file_size: usize = ((2<<20) as f64 * crate::test_scale()) as usize;
+    // A maximum write of 4 stripes should hit every special case in vdev_raid
+    let max_write_lbas = 4 * chunksize * (k - f) as LbaT;
+
+    let seed = seed.unwrap_or_else(|| {
+        let mut seed = [0u8; 16];
+        let mut seeder = thread_rng();
+        seeder.fill_bytes(&mut seed);
+        seed
+    });
+    println!("Using seed {:?}", &seed);
+    // Use XorShiftRng because it's deterministic and seedable
+    let mut rng = XorShiftRng::from_seed(seed);
+
+    // Do all the writes first
+    let mut nwritten = 0;
+    let zone = 0;
+    let zl = vdev.zone_limits(zone);
+    vdev.open_zone(zone).await.unwrap();
+    let mut ofs = zl.0;
+    let xfile = std::fs::File::create("/tmp/xfile.bin").unwrap();
+    while nwritten < file_size {
+        let write_lbas: LbaT = rng.gen_range(1..=max_write_lbas);
+        let write_bytes = write_lbas as usize * BYTES_PER_LBA;
+        let dbs = DivBufShared::from(mkbuf(ofs, write_bytes));
+        let wbuf = dbs.try_const().unwrap();
+        assert!(ofs + write_lbas < zl.1, "This test is not yet zone-aware");
+        xfile.write_at(&wbuf[..], ofs * BYTES_PER_LBA as u64).unwrap();
+        vdev.write_at(wbuf, zone, ofs).await.unwrap();
+        nwritten += write_bytes;
+        ofs += write_lbas;
+    }
+    // Don't close the zone so we'll retain an open StripeBuffer.
+
+    // Now read it back, with different offsets,and verify the contents.
+    ofs = zl.0;
+    let mut nread = 0;
+    while nread < nwritten {
+        let read_lbas: LbaT = rng.gen_range(1..=max_write_lbas);
+        let read_bytes = (nwritten - nread).min(read_lbas as usize * BYTES_PER_LBA);
+        let expect_buf = mkbuf(ofs, read_bytes);
+        let dbs = DivBufShared::from(vec![0u8; read_bytes]);
+        let rbuf = dbs.try_mut().unwrap();
+        assert!(ofs + read_lbas < zl.1, "This test is not yet zone-aware");
+        vdev.clone().read_at(rbuf, ofs).await.unwrap();
+        assert_eq!(&dbs.try_const().unwrap()[..], &expect_buf[..]);
+        nread += read_bytes;
+        ofs += read_lbas;
+    }
+}
+
+/// A RAID array with one missing disk
+#[rstest]
+// Stupid mirror
+#[case(harness(2, 2, 1, 1), None)]
+// Smallest possible PRIMES configuration
+#[case(harness(3, 3, 1, 2), None)]
+// Smallest PRIMES declustered configuration
+#[case(harness(5, 4, 1, 2), None)]
+// Smallest double-parity configuration
+#[case(harness(5, 5, 2, 2), None)]
+// Smallest non-ideal PRIME-S configuration
+#[case(harness(7, 4, 1, 2), None)]
+// Smallest triple-parity configuration
+#[case(harness(7, 7, 3, 2), None)]
+// Smallest quad-parity configuration
+#[case(harness(11, 9, 4, 2), None)]
+// Highly declustered configuration
+#[case(harness(7, 3, 1, 2), None)]
+#[awt]
+#[tokio::test]
+async fn degraded_1(
+    #[case] #[future] h: Harness,
+    #[case] seed: Option<[u8; 16]>,
+) {
+    let uuid = h.vdev.uuid();
+    drop(h.vdev);
+    fs::remove_file(h.paths[0].clone()).unwrap();
+    let mut manager = Manager::default();
+    for path in h.paths.iter() {
+        let _ = manager.taste(path).await;
+    }
+    let (vdev, _) = manager.import(uuid).await.unwrap();
+
+    do_test(vdev, h.chunksize, h.k, h.f, seed).await
+}
+
+/// A RAID array with two missing disks
+#[rstest]
+// Smallest double-parity configuration
+#[case(harness(5, 5, 2, 2), None)]
+// Smallest triple-parity configuration
+#[case(harness(7, 7, 3, 2), None)]
+// Smallest quad-parity configuration
+#[case(harness(11, 9, 4, 2), None)]
+// Highly declustered configuration
+#[case(harness(11, 4, 2, 2), None)]
+#[awt]
+#[tokio::test]
+async fn degraded_2(
+    #[case] #[future] h: Harness,
+    #[case] seed: Option<[u8; 16]>,
+) {
+    let uuid = h.vdev.uuid();
+    drop(h.vdev);
+    fs::remove_file(h.paths[0].clone()).unwrap();
+    fs::remove_file(h.paths[1].clone()).unwrap();
+    let mut manager = Manager::default();
+    for path in h.paths.iter() {
+        let _ = manager.taste(path).await;
+    }
+    let (vdev, _) = manager.import(uuid).await.unwrap();
+
+    do_test(vdev, h.chunksize, h.k, h.f, seed).await
+}
+
+/// A RAID array with three missing disks
+#[rstest]
+// Smallest triple-parity configuration
+#[case(harness(7, 7, 3, 2), None)]
+// Smallest quad-parity configuration
+#[case(harness(11, 9, 4, 2), None)]
+#[awt]
+#[tokio::test]
+async fn degraded_3(
+    #[case] #[future] h: Harness,
+    #[case] seed: Option<[u8; 16]>,
+) {
+    let uuid = h.vdev.uuid();
+    drop(h.vdev);
+    fs::remove_file(h.paths[0].clone()).unwrap();
+    fs::remove_file(h.paths[1].clone()).unwrap();
+    fs::remove_file(h.paths[2].clone()).unwrap();
+    let mut manager = Manager::default();
+    for path in h.paths.iter() {
+        let _ = manager.taste(path).await;
+    }
+    let (vdev, _) = manager.import(uuid).await.unwrap();
+
+    do_test(vdev, h.chunksize, h.k, h.f, seed).await
+}
+
+/// A healthy RAID array
 #[rstest]
 // Null RAID
 #[case(harness(1, 1, 0, 1), None)]
@@ -89,59 +241,11 @@ fn mkbuf(offs: LbaT, len: usize) -> Vec<u8> {
 #[case(harness(7, 3, 1, 2), None)]
 #[awt]
 #[tokio::test]
-async fn random_io(
+async fn healthy(
     #[case] #[future] h: Harness,
     #[case] seed: Option<[u8; 16]>,
 ) {
-    let file_size: usize = ((2<<20) as f64 * crate::test_scale()) as usize;
-    // A maximum write of 4 stripes should hit every special case in vdev_raid
-    let max_write_lbas = 4 * h.chunksize * (h.k - h.f) as LbaT;
-
-    let seed = seed.unwrap_or_else(|| {
-        let mut seed = [0u8; 16];
-        let mut seeder = thread_rng();
-        seeder.fill_bytes(&mut seed);
-        seed
-    });
-    println!("Using seed {:?}", &seed);
-    // Use XorShiftRng because it's deterministic and seedable
-    let mut rng = XorShiftRng::from_seed(seed);
-
-    // Do all the writes first
-    let mut nwritten = 0;
-    let zone = 0;
-    let zl = h.vdev.zone_limits(zone);
-    h.vdev.open_zone(zone).await.unwrap();
-    let mut ofs = zl.0;
-    let xfile = std::fs::File::create("/tmp/xfile.bin").unwrap();
-    while nwritten < file_size {
-        let write_lbas: LbaT = rng.gen_range(1..=max_write_lbas);
-        let write_bytes = write_lbas as usize * BYTES_PER_LBA;
-        let dbs = DivBufShared::from(mkbuf(ofs, write_bytes));
-        let wbuf = dbs.try_const().unwrap();
-        assert!(ofs + write_lbas < zl.1, "This test is not yet zone-aware");
-        xfile.write_at(&wbuf[..], ofs * BYTES_PER_LBA as u64).unwrap();
-        h.vdev.write_at(wbuf, zone, ofs).await.unwrap();
-        nwritten += write_bytes;
-        ofs += write_lbas;
-    }
-    // Don't close the zone so we'll retain an open StripeBuffer.
-
-    // Now read it back, with different offsets,and verify the contents.
-    ofs = zl.0;
-    let mut nread = 0;
-    while nread < nwritten {
-        let read_lbas: LbaT = rng.gen_range(1..=max_write_lbas);
-        let read_bytes = (nwritten - nread).min(read_lbas as usize * BYTES_PER_LBA);
-        let expect_buf = mkbuf(ofs, read_bytes);
-        let dbs = DivBufShared::from(vec![0u8; read_bytes]);
-        let rbuf = dbs.try_mut().unwrap();
-        assert!(ofs + read_lbas < zl.1, "This test is not yet zone-aware");
-        h.vdev.clone().read_at(rbuf, ofs).await.unwrap();
-        assert_eq!(&dbs.try_const().unwrap()[..], &expect_buf[..]);
-        nread += read_bytes;
-        ofs += read_lbas;
-    }
+    do_test(h.vdev, h.chunksize, h.k, h.f, seed).await
 }
 
 #[test]
